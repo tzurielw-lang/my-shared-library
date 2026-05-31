cfdc2ad7b6d689e7d133e0a74ae7b3aab4c5aeb1
Tzuriel Weisberg <tzurielw@jfrog.com>
2026-05-31 16:40:17 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function greet(name) {
+  return `Hello, ${name}! This message is coming directly from your JFrog library. Gusha`;
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+module.exports = { greet, add };
